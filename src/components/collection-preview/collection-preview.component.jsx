6968872fa63161c4ee9df68262ce253684ddff07
SrOlaff <polavo00@gmail.com>
2020-08-01 07:54:45 -0300

    Commit Shopping Preview

---
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./collection-preview.styles.scss";
+/*  aqui o que o cara fez foi bem inteligente, ele tirou do array shop usando o metodo de desconstrução para que possamos fazer o map
+apenas nos items que queremos, sem tocar no array SHOP_DATA, apenas pegando o items
+*/
+// Reminder: O filter é o seguinte, o primeiro parâmetro será o elemento, o segundo parâmetro será o indice dele dentro do array
+// que se encontra e o terceiro será o arary total
+
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <div key={item.id}>{item.name}</div>
+          ))}
+      </div>
+    </div>
+  );
+};
+export default CollectionPreview;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
-const HomePage = ({ history }) => {
+const HomePage = () => {
   // os parênteses substituem o uso do return, caso eu usasse o colchete teria que colocar o return
   return (
     <div className="homepage">
